0d62cb23c544d4cde05102478b6a98d3407d8d44
Elmer <eialfaro29@gmail.com>
2022-06-03 10:33:13 -0500

    base

---
+++ php/env.php
@@ -0,0 +1,5 @@
+; <?php exit; ?>
+MYSQL_HOST_NAME = "dbwebagile.crgntzlvbcit.us-east-2.rds.amazonaws.com"
+MYSQL_DATABASE_NAME = "test-react-php"
+MYSQL_USER = "admin"
+MYSQL_PASSWORD = "admin2903"
---
+++ react/src/Constantes.js
@@ -0,0 +1,5 @@
+const Constantes = {
+  //RUTA_API: "http://localhost/php",
+  RUTA_API: "https://test-react-php.herokuapp.com/php",
+};
+export default Constantes;
